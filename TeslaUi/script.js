0620fd47f2663b9c3e2a36c2e0bb67aa39cea235
Peter De bondt <peter@MacBook-Air-von-Peter.local>
2021-09-20 19:09:41 +0200

    Version1.3

--- TeslaUi/script.js
+++ TeslaUi/script.js
@@ -1,7 +1,40 @@
 
-function windshieldwipers (event) {
-	console.log ("hello")
-}
+const togglecard = () => {
+	document.querySelector(".cards")
+		.classList.toggle("hidden");
+};
 
-document.querySelector ("#temperature")
-	.addEventListener ("click", windshieldwipers);
+
+document.querySelector("#windshield-wipers")
+	.addEventListener("click", togglecard);
+
+
+
+const toggleavatar = () => {
+	document.querySelector(".avatar")
+	.classList.toggle("avatar-cards")
+};
+
+document.querySelector("#windshield-wipers")
+	.addEventListener("click", toggleavatar);
+
+
+
+
+const togglesettings = () => {
+	document.querySelector(".main-apps")
+		.classList.toggle("hidden");
+	document.querySelector(".maps")
+		.classList.toggle("hidden")
+
+	if ()
+};
+
+document.querySelector("#settings")
+	.addEventListener("click", togglesettings);
+
+
+
+
+
+
